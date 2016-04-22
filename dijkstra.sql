691399600dd50d47d54700ae5f77420606567607
ErickLF <y3rick_18@hotmail.com>
2016-04-22 14:03:12 -0700

    mas chilo

--- dijkstra.sql
+++ dijkstra.sql
@@ -1,49 +1,59 @@
-DROP TABLE IF EXISTS 'Nodo';
-CREATE TABLE 'Nodo' (
-  'id_nodo' int(11) NOT NULL AUTO_INCREMENT,
-  'name' varchar(1) NOT NULL,
-  PRIMARY KEY ('id_nodo'),
+
+DROP TABLE IF EXISTS Nodo;
+CREATE TABLE Nodo (
+  id_nodo int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(1) NOT NULL,
+  PRIMARY KEY (id_nodo)
 );
 
-DROP TABLE IF EXISTS 'Arista';
-CREATE TABLE 'Arista' (
-  'id_arista' int(11) NOT NULL AUTO_INCREMENT,
-  'nodo_a' int(11) NOT NULL,
-  'nodo_b' int(11) NOT NULL,
-  'peso' int(11) NOT NULL,
-  PRIMARY KEY ('id_arista'),
+DROP TABLE IF EXISTS Arista;
+CREATE TABLE Arista (
+  id_arista int(11) NOT NULL AUTO_INCREMENT,
+  nodo_a int(11) NOT NULL,
+  nodo_b int(11) NOT NULL,
+  peso int(11) NOT NULL,
+  PRIMARY KEY (id_arista)
 );
 
 
-CREATE FUNCTION dijkstra(@startNode int, @endNode int)
+CREATE FUNCTION dijkstra(startNode INT,endNode INT)
 RETURNS INT
 BEGIN
-  DECLARE paso int = 0;
+  DECLARE paso INT DEFAULT 0;   
   CREATE TEMPORARY TABLE plan(
-    'paso' int(11) NOT NULL,
-    'donde_viene' int(11) NOT NULL,
-    'nodo' int(11) NOT NULL,
-    'costo' int(11) NOT NULL,
-    FOREIGN KEY ('donde_viene') REFERENCES 'Nodo'('id_nodo'),
-    FOREIGN KEY ('nodo') REFERENCES 'Nodo'('id_nodo')
+    paso int(11) NOT NULL,
+    donde_viene int(11) NOT NULL,
+    nodo int(11) NOT NULL,
+    costo int(11) NOT NULL,
+    FOREIGN KEY (donde_viene) REFERENCES Nodo(id_nodo),
+    FOREIGN KEY (nodo) REFERENCES Nodo(id_nodo)
   );
 
   CREATE TEMPORARY TABLE camino(
-    'id' int(11) NOT NULL,
-    'nombre' varchar(25) NOT NULL,
-    'costo' int(11) NOT NULL,
-    PRIMARY KEY ('id');
+    id int(11) NOT NULL,
+    nombre varchar(25) NOT NULL,
+    costo int(11) NOT NULL,
+    PRIMARY KEY (id)
   );
 
   INSERT INTO plan VALUES (0,-1, startNode,0);
 
-  DECLARE fin boolean = startNode == endNode;
+  
   
   INSERT INTO camino (nombre,costo) VALUES (startNode,0);
+  
+  BEGIN
+      DECLARE nodoActual INT;
+      DECLARE papi INT;  
+      DECLARE costo INT;
+      DECLARE pasoActual INT;  
+      set nodoActual = StarNode;
 
-  DECLARE pasoActual int, nodoActual int = startNode, papi int, costo int;
-  DECLARE hijoActual int, costoHijo int;
-  WHILE !fin DO
+      DECLARE hijoActual INT;
+      DECLARE costoHijo int;  
+  END  
+
+  WHILE nodoActual != endNode DO
     DECLARE nodos CURSOR FOR SELECT * FROM 'plan' WHERE 'paso' = paso AND 'nodo' = nodoActual;
     LOOP
       FETCH nodos INTO pasoActual, papi, nodoActual, costo;
@@ -65,7 +75,6 @@ BEGIN
     FETCH mejor INTO nodoActual,costo;
 
     INSERT INTO camino (nombre,costo) VALUES (nodoActual,costo);
-    fin = nodoActual == endNode;
 
   END
 
