3edb5163c5c37dbe10590b114fb56145a86c9e43
ErickLF <y3rick_18@hotmail.com>
2016-04-22 13:32:22 -0700

    mejorado

--- dijkstra.sql
+++ dijkstra.sql
@@ -1,8 +1,3 @@
--- MySQL dump 10.13  Distrib 5.6.26, for Linux (x86_64)
---
--- Host: localhost    Database: e5ingsoft2
--- ------------------------------------------------------
-
 DROP TABLE IF EXISTS 'Nodo';
 CREATE TABLE 'Nodo' (
   'id_nodo' int(11) NOT NULL AUTO_INCREMENT,
@@ -19,36 +14,59 @@ CREATE TABLE 'Arista' (
   PRIMARY KEY ('id_arista'),
 );
 
+
 CREATE FUNCTION dijkstra(@startNode int, @endNode int)
 RETURNS INT
 BEGIN
-	DECLARE paso int = 0;
-	CREATE TEMPORARY TABLE plan(
-		'paso' int(11) NOT NULL,
-		'donde' int(11) NOT NULL,
-		'nodo' int(11) NOT NULL,
-		'costo' int(11) NOT NULL,
-		FOREIGN KEY ('donde') REFERENCES 'Nodo'('id_nodo'),
-		FOREIGN KEY ('nodo') REFERENCES 'Nodo'('id_nodo')
-	);
+  DECLARE paso int = 0;
+  CREATE TEMPORARY TABLE plan(
+    'paso' int(11) NOT NULL,
+    'donde_viene' int(11) NOT NULL,
+    'nodo' int(11) NOT NULL,
+    'costo' int(11) NOT NULL,
+    FOREIGN KEY ('donde_viene') REFERENCES 'Nodo'('id_nodo'),
+    FOREIGN KEY ('nodo') REFERENCES 'Nodo'('id_nodo')
+  );
 
-	INSERT INTO plan VALUES (0, -1, startNode, 0);
+  CREATE TEMPORARY TABLE camino(
+    'id' int(11) NOT NULL,
+    'nombre' varchar(25) NOT NULL,
+    'costo' int(11) NOT NULL,
+    PRIMARY KEY ('id');
+  );
 
-	DECLARE fin boolean = startNode == endNode;
-	DECLARE pasoActual int, nodoActual int, papi int, costo int;
-	WHILE !fin DO
-		DECLARE nodos CURSOR FOR SELECT 'nodo' FROM 'plan' WHERE 'paso' = paso;
-		LOOP
-			FETCH nodos INTO pasoActual, papi, nodoActual, costo;
-			DECLARE hijos CURSOR FOR SELECT * FROM 'Arista' WHERE 'nodo_a' = nodoActual;
-			
-			LOOP
-				FETCH hijos INTO
-			END LOOP
-		END LOOP;
+  INSERT INTO plan VALUES (0,-1, startNode,0);
 
-	END
+  DECLARE fin boolean = startNode == endNode;
+  
+  INSERT INTO camino (nombre,costo) VALUES (startNode,0);
+
+  DECLARE pasoActual int, nodoActual int = startNode, papi int, costo int;
+  DECLARE hijoActual int, costoHijo int;
+  WHILE !fin DO
+    DECLARE nodos CURSOR FOR SELECT * FROM 'plan' WHERE 'paso' = paso AND 'nodo' = nodoActual;
+    LOOP
+      FETCH nodos INTO pasoActual, papi, nodoActual, costo;
+      DECLARE hijos CURSOR FOR SELECT 'nodo_b', 'costo' FROM 'Arista' WHERE 'nodo_a' = nodoActual;
+      
+      LOOP
+        FETCH hijos INTO hijoActual, costoHijo;
+        INSERT INTO plan VALUES (paso+1, nodoActual, hijoActual, costo+costoHijo);
+      END LOOP
+    END LOOP  
+    DECLARE nodos2 CURSOR FOR SELECT * FROM 'plan' WHERE 'paso' = paso AND 'nodo' != nodoActual;
+    LOOP
+      FETCH nodos2 INTO pasoActual, papi, nodoActual, costo;
+      INSERT INTO plan VALUES (pasoActual+1, papi, nodoActual, costo);
+
+    END LOOP;
+    DECLARE mejor CURSOR FOR SELECT 'nodo', 'costo' FROM 'plan' WHERE 'paso' = paso ORDER BY 'costo';
+
+    FETCH mejor INTO nodoActual,costo;
+
+    INSERT INTO camino (nombre,costo) VALUES (nodoActual,costo);
+    fin = nodoActual == endNode;
+
+  END
 
 END
-
-
