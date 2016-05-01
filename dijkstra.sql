a8a2c3d272bc9f97e04d1afe0f96f83a71993e34
Nan Montaño <nano.mv112@gmail.com>
2016-04-30 19:02:05 -0700

    ejemplo Sonora listo y cambios en datos de tablas

--- dijkstra.sql
+++ dijkstra.sql
@@ -1,67 +1,73 @@
 DROP TABLE IF EXISTS Nodo;
 CREATE TABLE Nodo (
-  id_nodo varchar(1) NOT NULL,
-  PRIMARY KEY (id_nodo)
+  id_nodo int(11) NOT NULL AUTO_INCREMENT,
+  nombre varchar(20) NOT NULL,
+  PRIMARY KEY (id_nodo),
+  UNIQUE KEY name (nombre)
 );
 
   
 DROP TABLE IF EXISTS Arista;
 CREATE TABLE Arista (
   id_arista int(11) NOT NULL AUTO_INCREMENT,
-  nodo_a char(1) NOT NULL,
-  nodo_b char(1) NOT NULL,
-  costo int(11) NOT NULL,
+  nodo_a int(11) NOT NULL,
+  nodo_b int(11) NOT NULL,
+  costo float(10,2) NOT NULL,
   PRIMARY KEY (id_arista)
 );
 
 DROP PROCEDURE IF EXISTS dijkstra;
 
 DELIMITER //
-CREATE PROCEDURE dijkstra (startNode CHAR,endNode CHAR)
+CREATE PROCEDURE dijkstra (startNode VARCHAR(20),endNode VARCHAR(20))
 BEGIN
-  DECLARE paso,costo,costoHijo,costoAux INT DEFAULT 0;
-  DECLARE nodoActual,papi,hijoActual CHAR;
-  SET nodoActual = startNode; 
+  DECLARE costo,costoHijo,costoAux FLOAT DEFAULT 0;
+  DECLARE startId, endId, paso,nodoActual,papi,hijoActual INT DEFAULT 0;
+  SELECT Nodo.id_nodo FROM Nodo WHERE Nodo.nombre = startNode INTO startId;
+  SELECT Nodo.id_nodo FROM Nodo WHERE Nodo.nombre = endNode INTO endId;
+  SET nodoActual = startId;
   
   DROP TABLE IF EXISTS plan;
   CREATE TEMPORARY TABLE plan (
     #paso int(11) NOT NULL,
-    seleccionado boolean DEFAULT FALSE,
-    dondeViene char(1) DEFAULT NULL,
-    nodo char(1) NOT NULL,
-    costo int(11) NOT NULL DEFAULT 1024
+    seleccionado int(11) DEFAULT -1,
+    dondeViene int(11) DEFAULT NULL,
+    nodo int(11) NOT NULL,
+    costo float(12,2) NOT NULL DEFAULT 999999999.99 #max signed int
   );
   
   DROP TABLE IF EXISTS camino; 
   CREATE TEMPORARY TABLE camino(
-    de char(1) NOT NULL,
-    a char(1) NOT NULL,
-    costo int(11) NOT NULL
+    de varchar(20) NOT NULL,
+    a varchar(20) NOT NULL,
+    costo float(12,2) NOT NULL
   );
   
   
   BEGIN
-    DECLARE nodoName CHAR;
+    DECLARE nodoId INT;
     DECLARE initListo INT DEFAULT FALSE;
     DECLARE init CURSOR FOR SELECT Nodo.id_nodo FROM Nodo 
-        WHERE Nodo.id_nodo != startNode;
+        WHERE Nodo.id_nodo != startId;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET initListo = TRUE;
+    #inserta el nodo inciial
     INSERT INTO plan (plan.seleccionado, plan.nodo, plan.costo) 
-      VALUES(TRUE, startNode, 0);
+      VALUES(0, startId, 0);
     OPEN init;
     loopinit: LOOP
-      FETCH NEXT FROM init INTO nodoName;
+      FETCH NEXT FROM init INTO nodoId;
       IF initListo THEN
           LEAVE loopinit;
       END IF;
-      INSERT INTO plan (plan.nodo) VALUES (nodoName);
+      INSERT INTO plan (plan.nodo) VALUES (nodoId);
     END LOOP;
     CLOSE init;
   END;
-  
   #nomas por si se cicla
   guail: WHILE paso < 10000 DO
     #sacamos los datos del nodo actual
+    #SELECT * FROM plan;
+    SET paso = paso+1;
     SELECT plan.dondeViene, plan.costo FROM plan WHERE plan.nodo = nodoActual
       INTO papi, costo;
     BEGIN
@@ -87,29 +93,35 @@ BEGIN
       END LOOP;
       CLOSE hijos;
     END;  
-    
-  SET paso = paso+1;
+  
   BEGIN
-    DECLARE nextCursor CURSOR FOR SELECT plan.dondeViene, plan.nodo, plan.costo FROM plan 
-      WHERE plan.seleccionado = FALSE ORDER BY plan.costo;
+    DECLARE nextCursor CURSOR FOR SELECT plan.nodo FROM plan 
+      WHERE plan.seleccionado = -1 ORDER BY plan.costo;
     OPEN nextCursor;
-    FETCH NEXT FROM nextCursor INTO papi, nodoActual, costo;
+    FETCH NEXT FROM nextCursor INTO nodoActual;
     CLOSE nextCursor;
-    UPDATE plan SET plan.seleccionado = TRUE WHERE plan.nodo = nodoActual;
-    IF nodoActual = endNode THEN
+    UPDATE plan SET plan.seleccionado = paso WHERE plan.nodo = nodoActual;
+    IF nodoActual = endId THEN
       LEAVE guail;
     END IF;
   END;
   END WHILE;  
   
-  WHILE nodoActual != startNode DO
-    SELECT plan.dondeViene, plan.costo FROM plan 
-      WHERE plan.nodo = nodoActual INTO papi, costo;
-    INSERT INTO camino VALUES (papi, nodoActual, costo);
-    SET nodoActual = papi;
-  END WHILE;
+  BEGIN
+    DECLARE actualName, papiName VARCHAR(20);
+    WHILE nodoActual != startId DO
+        SELECT plan.dondeViene, plan.costo FROM plan 
+        WHERE plan.nodo = nodoActual INTO papi, costo;
+        SELECT Nodo.nombre FROM Nodo WHERE Nodo.id_nodo = nodoActual INTO actualName;
+        SELECT Nodo.nombre FROM Nodo WHERE Nodo.id_nodo = papi INTO papiName;
+        INSERT INTO camino VALUES (papiName, actualName, costo);
+        SET nodoActual = papi;
+    END WHILE;
+  END;
     
-  SELECT * FROM plan;
+#debug
   SELECT * FROM camino ORDER BY camino.costo;
+  DROP TABLE camino;
+  DROP TABLE plan;
 END//
 DELIMITER ;
--- ejemplo_grafo.sql
+++ ejemplo_grafo.sql
@@ -1,24 +1,53 @@
-DROP PROCEDURE IF EXISTS crear_grafo;
+
+DELETE FROM Nodo;
+DELETE FROM Arista;
+
+INSERT INTO Nodo (nombre) VALUES ('Hermosillo');    #1
+INSERT INTO Nodo (nombre) VALUES ('Santa Ana');     #2
+INSERT INTO Nodo (nombre) VALUES ('Nogales');       #3
+INSERT INTO Nodo (nombre) VALUES ('Caborca');       #4
+INSERT INTO Nodo (nombre) VALUES ('Puerto Peñasco');#5
+INSERT INTO Nodo (nombre) VALUES ('Cananea');       #6
+INSERT INTO Nodo (nombre) VALUES ('Agua Prieta');   #7
+INSERT INTO Nodo (nombre) VALUES ('Bahia de Kino'); #8
+INSERT INTO Nodo (nombre) VALUES ('Guaymas');       #9
+INSERT INTO Nodo (nombre) VALUES ('Obregon');       #10
+INSERT INTO Nodo (nombre) VALUES ('Yecora');        #11
+INSERT INTO Nodo (nombre) VALUES ('Navojoa');       #12
+INSERT INTO Nodo (nombre) VALUES ('Huatabampo');    #13
+INSERT INTO Nodo (nombre) VALUES ('Alamos');        #14
+INSERT INTO Nodo (nombre) VALUES ('Ures');          #15
+
+DROP PROCEDURE IF EXISTS addArista;
 
 DELIMITER //
-CREATE PROCEDURE crear_grafo (startNode CHAR,endNode CHAR)
+
+CREATE PROCEDURE addArista(aId INT, bId INT, costo FLOAT)
 BEGIN
-
-    INSERT INTO Nodo VALUES ('a');
-    INSERT INTO Nodo VALUES ('b');
-    INSERT INTO Nodo VALUES ('c');
-    INSERT INTO Nodo VALUES ('d');
-    INSERT INTO Nodo VALUES ('e');
-    INSERT INTO Nodo VALUES ('f');
-
-
-    INSERT INTO Arista (nodo_a, nodo_b, costo) VALUES ('a', 'b', 3); # a--(3)--> b
-    INSERT INTO Arista (nodo_a, nodo_b, costo) VALUES ('a', 'c', 1); # a--(1)--> c
-    INSERT INTO Arista (nodo_a, nodo_b, costo) VALUES ('b', 'd', 1); # b--(1)--> d
-    INSERT INTO Arista (nodo_a, nodo_b, costo) VALUES ('c', 'e', 2); # c--(2)--> e
-    INSERT INTO Arista (nodo_a, nodo_b, costo) VALUES ('d', 'f', 1); # d--(1)--> f
-    INSERT INTO Arista (nodo_a, nodo_b, costo) VALUES ('e', 'f', 5); # e--(5)--> f
-
+    INSERT INTO Arista (nodo_a, nodo_b, costo) VALUES(aId, bId, costo);
+    INSERT INTO Arista (nodo_a, nodo_b, costo) VALUES(bId, aId, costo);
 END//
 
 DELIMITER ;
+
+CALL addArista(1, 2, 173);
+CALL addArista(1, 15, 79);
+CALL addArista(1, 8, 108);
+CALL addArista(1, 9, 135);
+CALL addArista(1, 10, 250);
+CALL addArista(1, 11, 281);
+CALL addArista(2, 3, 107);
+CALL addArista(2, 4, 112);
+CALL addArista(2, 6, 126);
+CALL addArista(3, 6, 146);
+CALL addArista(4, 5, 107);
+CALL addArista(6, 7, 86);
+CALL addArista(6, 15, 244);
+CALL addArista(7, 11, 456);
+CALL addArista(7, 15, 300);
+CALL addArista(9, 10, 128);
+CALL addArista(10, 11, 215);
+CALL addArista(10, 12, 67);
+CALL addArista(10, 13, 100);
+CALL addArista(12, 13, 37);
+CALL addArista(12, 14, 52);
